75e7cb9f3a96537cc00cbfe4bda54caab9e98c3b
parminder <parminder@leo-MS-7636.(none)>
2013-07-23 09:20:52 +0530

    commit to pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(:version => 20130722110821) do
 
-=======
-ActiveRecord::Schema.define(:version => 20130722103807) do
 
   create_table "keys", :force => true do |t|
     t.string   "generated_key"
@@ -23,7 +21,7 @@ ActiveRecord::Schema.define(:version => 20130722103807) do
     t.datetime "updated_at",    :null => false
   end
 
->>>>>>> a776f41715ccd2ea6339e243ef3869e4b6102999
+
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password"
