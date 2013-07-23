db6fb1e935b5ab5793f8dc3810988f278f7f59bb
parminder <parminder@leo-MS-7636.(none)>
2013-07-23 09:15:03 +0530

    user controller changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,24 +86,5 @@ class UsersController < ApplicationController
     end
   end
 
-  def generate_keys
-    @error=false
-    @generated_list=[]
-    2.times do
-      @list=SecureRandom.hex(8)
-      @generated_list<<@list      
-      @key=Key.new(:generated_key => @list)
-      if @key.save
-
-        else
-        @error=true
-      end
-    end
-    unless @error
-      render :text=> "error"
-    else
-      render :text=> "created"
-    end
-  end
-
+  
 end
